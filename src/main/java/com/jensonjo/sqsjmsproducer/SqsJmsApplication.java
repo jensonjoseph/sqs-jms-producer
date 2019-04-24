de3a44e5a3931c91934be0079f8df4e0628ee347
jensonjoseph <jenson.joseph@outlook.com>
2019-04-24 15:09:25 -0400

    converting to rest endpoint

--- src/main/java/com/jensonjo/sqsjmsproducer/SqsJmsApplication.java
+++ src/main/java/com/jensonjo/sqsjmsproducer/SqsJmsApplication.java
@@ -24,43 +24,7 @@ public class SqsJmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SqsJmsApplication.class, args);
 
-//        log.info("Create a new connection factory with all defaults (credentials and region) set automatically");
-        SQSConnectionFactory connectionFactory = new SQSConnectionFactory(
-                new ProviderConfiguration(),
-                AmazonSQSClientBuilder.defaultClient()
-        );
 
-        try {
-//            log.info("Create the connection");
-            SQSConnection connection = connectionFactory.createConnection();
-
-//            log.info("Get the wrapped client");
-            AmazonSQSMessagingClientWrapper client = connection.getWrappedAmazonSQSClient();
-
-//            log.info("Create an SQS queue named MyQueue, if it doesn't already exist");
-            if (!client.queueExists("MyQueue")) {
-                client.createQueue("MyQueue");
-            }
-
-//            log.info("Create the nontransacted session with AUTO_ACKNOWLEDGE mode");
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-//            log.info("Create a queue identity and specify the queue name to the session");
-            Queue queue = session.createQueue("MyQueue");
-
-//            log.info("Create a producer for the 'MyQueue'");
-            MessageProducer producer = session.createProducer(queue);
-
-//            log.info("Create the text message");
-            TextMessage message = session.createTextMessage("Hello World");
-
-//            log.info("Send the message");
-            producer.send(message);
-            log.info("JMS Message " + message.getJMSMessageID());
-
-        } catch (JMSException e) {
-            log.severe(e.getLocalizedMessage());
-        }
 
 
     }
